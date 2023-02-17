c8cc587fb1d65f5ac205e971a1e62c0584dddece
Fabrizio Tamma <>
2023-02-17 09:55:59 +0100

    1.0

---
+++ sito/static/js/script.js
@@ -0,0 +1,46 @@
+let grid = [
+[null,null,null],
+[null,null,null],
+[null,null,null]
+]
+
+
+
+let links = document.querySelectorAll(".gridBtn"),
+currentPlayer = 1,
+    cellIsOccupied = true
+
+
+for(let i = 0; i < links.length; i++){
+let link = links[i]
+link.addEventListener("click", function(e){
+e.preventDefault()
+        cellIsOccupied = e.currentTarget.classList.contains("player2") || e.currentTarget.classList.contains("player1") ;
+       console.log(cellIsOccupied)
+if(cellIsOccupied === false){
+
+console.log(e.currentTarget.dataset)
+         e.currentTarget.classList.toggle("player"+currentPlayer)
+
+         let riga=e.currentTarget.dataset.row
+         let colonna=e.currentTarget.dataset.col
+
+         if(currentPlayer === 1){
+          grid[riga][colonna]="X"
+         }else{
+          grid[riga][colonna]="O"
+         }
+
+
+         if(currentPlayer === 1){
+           currentPlayer = 2
+         }else{
+           currentPlayer = 1
+         }
+       }
+})
+}
+
+reset.addEventListener("click", function(event){
+  window.location.reload()
+})
